138b1aba2b405c0df0ddd92913668505d7086d92
MarioLndz <mariolindez@gmail.com>
2023-03-29 19:57:27 +0200

    Initial Commit

---
+++ include/punto.h
@@ -0,0 +1,21 @@
+//
+// Created by mario on 29/03/23.
+//
+
+#ifndef PUNTO_H
+#define PUNTO_H
+
+class Punto {
+private:
+    int x;
+    int y;
+
+public:
+    Punto(int la_x, int la_y);
+
+
+
+};
+
+#include "punto.cpp"
+#endif //PUNTO_H
---
+++ src/punto.cpp
@@ -0,0 +1,8 @@
+//
+// Created by mario on 29/03/23.
+//
+
+#include "punto.h"
+#include <iostream>
+
+using namespace std;
