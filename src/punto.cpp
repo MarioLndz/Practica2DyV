07d4ba986a645a2e5fab80e6b5ed40d05678d909
MarioLndz <mariolindez@gmail.com>
2023-03-29 20:27:22 +0200

    Initial Commit

--- src/punto.cpp
+++ src/punto.cpp
@@ -33,9 +33,28 @@ int Punto::getCuadrante() {
 bool Punto::esMenor(Punto otro) {
     bool es_menor = false;
 
-    if ()
-    if (this->getCuadrante() <= otro.getCuadrante()){
-        es_menor = true;
+    int cuadrante = getCuadrante();
+
+    if (cuadrante != otro.getCuadrante()){  // Distinto Cuadrante
+        if (this->getCuadrante() < otro.getCuadrante()){
+            es_menor = true;
+        }
+    } else {    // Mismo cuadrante
+        switch (cuadrante) {
+            case (1):
+
+                break;
+            case (2):
+                break;
+            case (3):
+                break;
+            case (4):
+                break;
+            default:
+                es_menor = true;
+        }
+
     }
+
 
 }
