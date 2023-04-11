aeadfe9f1dd8e635fd9b1266500d04736217eb0e
MarioLndz <mariolindez@gmail.com>
2023-04-11 17:11:37 +0200

    .

--- mide-tiempos.sh
+++ mide-tiempos.sh
@@ -3,7 +3,11 @@
 i=50000
 while [ "$i" -le 500000 ]
 do
-    ./Especifico data.txt $i >> salidaE.dat
+	# Generamos los puntos	
+	./generador $1
+  	
+	# Ejecutamos los puntos
+	./especifico data.txt $i >> salidaE.dat
       i=$(( $i + 50000 ))
 done
       
